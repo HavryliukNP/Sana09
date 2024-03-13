424a4922a6af65b169bac695c27d22db930fc754
Nazarii Havryliuk <ipz234_gnp@student.ztu.edu.ua>
2024-03-13 12:11:41 +0200

    Виконав завдання Sana09

---
+++ Sana09/Controllers/GlobalController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sana09.Models;
+using System.Diagnostics;
+
+namespace Sana09.Controllers
+{
+    public class GlobalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Action1()
+        {
+            return View();
+        }
+        public IActionResult Action2()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
